fb45ebb465076e6ed928b6aa8597168613364ac5
mike <mike@place.(none)>
2011-06-13 19:44:10 -0500

    Blog acabado primera fase

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nblog::Application.config.secret_token = '3f0f78ed426dd43bf9cec26ad6dd90789bc7441d7c6f10a54fb52106b9ab2cbffe9e982a41825152900487ec2c7f765729d15ce1aced048469d410d25abf9220'
